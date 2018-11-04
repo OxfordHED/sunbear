fd535b0f67ba999df1e10a51ff25dd719b197965
mfkasim91 <firman.kasim@gmail.com>
2018-11-04 18:36:44 +0000

    TST: Test suite for the forward_pos function

--- sunbear/test/forward_test.py
+++ sunbear/test/forward_test.py
@@ -18,6 +18,18 @@ def test_forward_3d_no_pot():
 def test_forward_4d_no_pot():
     _test_forward_nd_no_pot(n=10, ndim=4)
 
+def test_forward_pos_1d_no_pot():
+    _test_forward_pos_nd_no_pot(n=100, npts=100, ndim=1)
+
+def test_forward_pos_2d_no_pot():
+    _test_forward_pos_nd_no_pot(n=100, npts=100, ndim=2)
+
+def test_forward_pos_3d_no_pot():
+    _test_forward_pos_nd_no_pot(n=100, npts=100, ndim=3)
+
+def test_forward_pos_4d_no_pot():
+    _test_forward_pos_nd_no_pot(n=20, npts=100, ndim=4)
+
 def _test_forward_nd_no_pot(n, ndim):
     x = np.linspace(-1, 1, n)
     xs = np.meshgrid([x]*ndim, indexing="ij")
@@ -34,6 +46,19 @@ def _test_forward_nd_no_pot(n, ndim):
     # make sure the source is not changed
     assert np.all(source == source_copy)
 
+def _test_forward_pos_nd_no_pot(n, npts, ndim):
+    np.random.seed(123)
+    source_pos = np.random.random((npts, ndim)) * (n-1)
+    source_pos_copy = np.copy(source_pos)
+    phi = np.zeros([n]*ndim)
+
+    # target should be the same as source, as there's no potential
+    target_pos = sb.forward_pos(source_pos, phi)
+    assert source_pos == pytest.approx(target_pos)
+
+    # make sure the source is not changed
+    assert np.all(source_pos == source_pos_copy)
+
 ########################### case: slanted potential ###########################
 def test_forward_1d_slanted_pot():
     _test_forward_nd_slanted_pot(n=100, ndim=1)
@@ -43,6 +68,15 @@ def test_forward_2d_slanted_pot():
 
 def test_forward_3d_slanted_pot():
     _test_forward_nd_slanted_pot(n=20, ndim=3)
+
+def test_forward_pos_1d_slanted_pot():
+    _test_forward_pos_nd_slanted_pot(n=100, npts=100, ndim=1)
+
+def test_forward_pos_2d_slanted_pot():
+    _test_forward_pos_nd_slanted_pot(n=100, npts=100, ndim=2)
+
+def test_forward_pos_3d_slanted_pot():
+    _test_forward_pos_nd_slanted_pot(n=20, npts=100, ndim=3)
 
 def _test_forward_nd_slanted_pot(n, ndim, abs=None):
     x = np.arange(n) - n / 2.0
@@ -73,6 +107,33 @@ def _test_forward_nd_slanted_pot(n, ndim, abs=None):
     # make sure the source is not changed
     assert np.all(source == source_copy)
 
+def _test_forward_pos_nd_slanted_pot(n, npts, ndim):
+    x = np.arange(n) - n / 2.0
+    xs = np.meshgrid(*([x]*ndim), indexing="ij")
+
+    # phi is slanted on the first dimension, so the target will be shifted
+    # source on the first dimension by A
+    A = n / 6.0
+    sigma = (n/30.)
+    phi = xs[0] * A
+
+    # test the interior and edge points
+    # The edge points will have the same displacement as the interior points
+    # next to it, so it will also be displaced by the same amount as the
+    # interior points.
+    np.random.seed(123)
+    source_pos = np.random.random((npts, ndim)) * (n-1)
+    source_pos_copy = np.copy(source_pos)
+    target_pos = sb.forward_pos(source_pos, phi)
+
+    target_pos_calc = source_pos + np.array([A]+[0]*(ndim-1))
+
+    # compare the target position
+    assert target_pos == pytest.approx(target_pos_calc)
+
+    # make sure the source is not changed
+    assert np.all(source_pos == source_pos_copy)
+
 ########################## case: quadratic potential ##########################
 def test_forward_1d_quad_pot():
     _test_forward_nd_quad_pot(n=100, ndim=1)
@@ -82,6 +143,15 @@ def test_forward_2d_quad_pot():
 
 def test_forward_3d_quad_pot():
     _test_forward_nd_quad_pot(n=20, ndim=3)
+
+def test_forward_pos_1d_quad_pot():
+    _test_forward_pos_nd_quad_pot(n=100, npts=100, ndim=1)
+
+def test_forward_pos_2d_quad_pot():
+    _test_forward_pos_nd_quad_pot(n=100, npts=100, ndim=2)
+
+def test_forward_pos_3d_quad_pot():
+    _test_forward_pos_nd_quad_pot(n=20, npts=100, ndim=3)
 
 def _test_forward_nd_quad_pot(n, ndim, abs=None):
     x = np.arange(n) - n / 2.0
@@ -109,3 +179,28 @@ def _test_forward_nd_quad_pot(n, ndim, abs=None):
 
     # make sure the source is not changed
     assert np.all(source == source_copy)
+
+def _test_forward_pos_nd_quad_pot(n, npts, ndim, abs=None):
+    x = np.arange(n) - n / 2.0
+    xs = np.meshgrid(*([x]*ndim), indexing="ij")
+    xs_sq = reduce(lambda x,y:x+y*y, xs, 0.0)
+
+    # phi is quadratic on all dimension, so the target will be scaled
+    # source on all dimension by (1+B)
+    B = 1.0
+    scale = 1 + B
+    sigma = (n/30.)
+    phi = 0.5*B*xs_sq
+
+    # test the interior points
+    np.random.seed(123)
+    source_pos = np.random.random((npts, ndim)) * (n-3) + 1
+    source_pos_copy = np.copy(source_pos)
+    target_pos = sb.forward_pos(source_pos, phi)
+
+    # the displacement is x*B where x is (pos-n/2.0)
+    target_pos_calc = (source_pos-n/2.0)*B + source_pos
+    assert target_pos == pytest.approx(target_pos_calc)
+
+    # make sure the source is not changed
+    assert np.all(source_pos == source_pos_copy)
