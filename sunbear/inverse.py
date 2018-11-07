6f1db978b725f6ec713035acf370154d564a9765
mfkasim91 <firman.kasim@gmail.com>
2018-11-07 15:56:30 -0800

    MAINT: Using map_coordinates instead of RegularGridInterpolator inverse

--- sunbear/inverse.py
+++ sunbear/inverse.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.interpolate import RegularGridInterpolator
+from scipy.ndimage import map_coordinates
 from sunbear.math.diff import grad, det_hess
 from sunbear.forward import _get_full_potential, _get_idx
 from sunbear.gradopt import Momentum
@@ -49,7 +49,7 @@ def inverse(source, target, gradopt_obj=None, interp="linear"):
     # initialize phi0
     ndim = np.ndim(source)
     phi0 = np.zeros_like(source)
-    u0, _, phi0_pad = _get_full_potential(phi0)
+    u0, _, phi0_pad = _get_full_potential(phi0, -1)
     x = np.array([grad(u0, axis=i) for i in range(ndim)])
     pts = tuple([xx[_get_idx(ndim, i, slice(None,None,None), 0)] \
         for i,xx in enumerate(x)])
@@ -57,9 +57,8 @@ def inverse(source, target, gradopt_obj=None, interp="linear"):
     # functions to get the loss function and the gradient
     idx_interior = tuple([slice(3,-3,None)]*ndim)
     idx_interior2 = tuple([slice(2,-2,None)]*ndim)
-    target_shape = target.shape
-    interpolator = RegularGridInterpolator(pts, target, method=interp)
     log_source = np.log(np.abs(source))
+    order = {"nearest": 0, "linear": 1, "quadratic": 2}[interp]
 
     def grad_phi_pad(phi_pad):
         u = u0 + phi_pad
@@ -67,12 +66,11 @@ def inverse(source, target, gradopt_obj=None, interp="linear"):
         # calculate the determinant of the hessian
         det_hess_s = det_hess(u)
 
-        # get the new position in (n x D) format
-        y = np.array([grad(u , axis=i) for i in range(ndim)]) # (D x n x n)
-        ypts = y.reshape(ndim, -1).T
+        # get the new position
+        ypts = np.array([grad(u, axis=i) for i in range(ndim)]) # (D x n x n)
 
         # get the target density on the source plane
-        target_s = interpolator(ypts).reshape(target_shape)
+        target_s = map_coordinates(target, ypts, order=order, mode="constant")
 
         # calculate the dudt based on Sulman (2011)
         dudt_interior = log_source - np.log(np.abs(target_s * det_hess_s))
